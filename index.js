906432daa7990dee82375e84be9bb051bf8d0d55
Alex Dobrushskiy <a.dobrushskiy@gmail.com>
2014-05-12 04:28:02 +0000

    Reading response from index.html

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  var filebuff = fs.readFileSync('index.html'); 
+  var filestring = filebuff.toString();
+  response.send(filestring);
 })
 
 app.listen(app.get('port'), function() {
